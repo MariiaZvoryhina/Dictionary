a329e99e7790b1fdbab6bdaead714d2172f061af
MariiaZvoryhina <zvorygina.masha@gmail.com>
2022-10-30 12:21:43 +0200

    Added word meanings

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import logo from "./dictionary-logo.png";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App text-center">
       <div className="container">
         <header className="App-header">
           <img src={logo} alt="logo" className="img-fluid logo " />
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
-  function handleResponse(response) {
-    console.log(response);
+  let [answear, setAnswear] = useState(null);
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
   }
 
   function search(event) {
@@ -13,14 +16,17 @@ export default function Dictionary() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
-  function handleKeywordChange(event) {
-    setKeyword(event.target.value);
+
+  function handleResponse(response) {
+    setAnswear(response.data[0]);
   }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={answear} />
     </div>
   );
 }
