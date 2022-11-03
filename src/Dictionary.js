36580b7cbfb614c24c5656525aae5a3adee60836
MariiaZvoryhina <zvorygina.masha@gmail.com>
2022-11-03 14:05:05 +0200

    Add photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,19 @@ import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [result, setResult] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
+  }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +22,10 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let pexelsApiKey = `563492ad6f9170000100000120f3ed306b7148308bfcb9582039e797`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleResponse(response) {
@@ -42,6 +51,7 @@ export default function Dictionary() {
           <small class="hint">i.e. paris, wine, yoga, coding</small>
         </section>
         <Results results={result} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
